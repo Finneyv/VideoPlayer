45719235e518fc81f4620b0cfb539f9ac2c8fa9c
Victor Finney <finneyv09@gmail.com>
2016-10-27 16:57:11 -0400

    Initial commit

---
+++ VideoplayerTest/VideoPlayerApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace VideoplayerTest
+{
+	public class VideoPlayerApp : Application
+	{
+		public VideoPlayerApp()
+		{
+			NavigationPage navPage = new NavigationPage(new VideoPlayerPage());
+
+			MainPage = navPage;
+		}
+
+		protected override void OnStart()
+		{
+			base.OnStart();
+		}
+
+		protected override void OnSleep()
+		{
+			base.OnSleep();
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+		}
+	}
+}
+
---
+++ VideoplayerTest/VideoPlayerPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace VideoplayerTest
+{
+	public partial class VideoPlayerPage : ContentPage
+	{
+		public VideoPlayerPage()
+		{
+			InitializeComponent();
+
+
+		}
+	}
+}
